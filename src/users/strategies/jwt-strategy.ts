6d80c17c097a2249bc6a060c964ffcea442fd332
Tarek Yahia <eng.tarek.yahiia@gmail.com>
2023-12-10 11:21:38 +0200

    Car Vehicles Estimate Value - Nestjs Project

--- src/users/strategies/jwt-strategy.ts
+++ src/users/strategies/jwt-strategy.ts
@@ -6,7 +6,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "SecretJwtTokens",
+      secretOrKey: process.env.jwt_secret,
     });
   }
 
