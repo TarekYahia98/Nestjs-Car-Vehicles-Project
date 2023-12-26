9d4157aff58c4860a830c6e71dbcbbe98a1d8e94
Tarek Yahia <eng.tarek.yahiia@gmail.com>
2023-12-26 12:39:38 +0200

    Car Vehicles Estimate Value - Nestjs Project

--- src/reports/dtos/approve-report.dto.ts
+++ src/reports/dtos/approve-report.dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean } from "class-validator";
 
 
 export class ApproveReportDto {
     
     @IsBoolean()
+    @ApiProperty({ type: Boolean, description: "approved"})
     approved: boolean;
 }
--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail,IsNotEmpty,IsString, MaxLength, MinLength} from "class-validator";
 
 export class createUserDto {
@@ -6,11 +7,13 @@ export class createUserDto {
   @IsEmail()
   @MinLength(5)
   @MaxLength(50)
+  @ApiProperty({ type: String, description: "email"})
   email: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(5)
   @MaxLength(30)
+  @ApiProperty({ type: String, description: "password"})
   password: string;
 }
